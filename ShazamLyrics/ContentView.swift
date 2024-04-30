075242439857cb771dcf5c0d2ac36ab266f15bfc
Enrique Sarmiento <jeisone.sarmiento@gmail.com>
2024-04-30 19:13:07 -0400

    Initial Commit

---
+++ ShazamLyrics/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ShazamLyrics
+//
+//  Created by Enrique Sarmiento on 30/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ ShazamLyrics/ShazamLyricsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShazamLyricsApp.swift
+//  ShazamLyrics
+//
+//  Created by Enrique Sarmiento on 30/4/24.
+//
+
+import SwiftUI
+
+@main
+struct ShazamLyricsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
